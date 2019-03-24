db43e73669c55ed2a2d79ffa608effedf65db096
Rafael de Oliveira <rafaextradev@gmail.com>
2019-03-24 05:25:19 -0300

    update: Repository comp.

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Container, Repository } from './styles';
 
 const CompareList = () => (
-  <div>
-    <div>
+  <Container>
+    <Repository>
       <header>
         <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="Facebook" />
         <strong>react</strong>
@@ -27,8 +28,8 @@ const CompareList = () => (
           <small>last commit</small>
         </li>
       </ul>
-    </div>
-  </div>
+    </Repository>
+  </Container>
 );
 
 export default CompareList;
